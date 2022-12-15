bac7b7355dfedd8e8b67254b228ba0ebbec3ddf4
Scott <>
2022-12-15 02:03:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+print (f"Yo, is it chilly in here or is it just me?")
+import time
+time.sleep (2) # sleep for 2 seconds
+print (f"I think it might just be me....")
+import time
+time.sleep (2) # sleep for 2 seconds
+print (f"Yo WTF IS THAT!?!?!?")
+import time
+time.sleep (1) # sleep for 2 seconds
+print (f"........")
+import time
+time.sleep (2) # sleep for 1 second
+print (f"""░░░░░░░░▄▄▄▀▀▀▄▄███▄░░░░░░░░░░░░░░
+░░░░░▄▀▀░░░░░░░▐░▀██▌░░░░░░░░░░░░░
+░░░▄▀░░░░▄▄███░▌▀▀░▀█░░░░░░░░░░░░░
+░░▄█░░▄▀▀▒▒▒▒▒▄▐░░░░█▌░░░░░░░░░░░░
+░▐█▀▄▀▄▄▄▄▀▀▀▀▌░░░░░▐█▄░░░░░░░░░░░
+░▌▄▄▀▀░░░░░░░░▌░░░░▄███████▄░░░░░░
+░░░░░░░░░░░░░▐░░░░▐███████████▄░░░
+░░░░░le░░░░░░░▐░░░░▐█████████████▄
+░░░░toucan░░░░░░▀▄░░░▐█████████████▄ 
+░░░░░░has░░░░░░░░▀▄▄███████████████ 
+░░░░░arrived░░░░░░░░░░░░█▀██████░░""")
